1f367018d75286c3d6b4e808facc6e9fb5fe77db
memoizr <memoizrlabs@gmail.com>
2019-04-23 14:02:20 +0100

    Make ShankModule mehtods extension methods

--- src/main/kotlin/com/memoizr/ShankModule.kt
+++ src/main/kotlin/com/memoizr/ShankModule.kt
@@ -4,27 +4,26 @@ import com.memoizr.ShankFactoryCache.factories
 
 
 interface ShankModule {
-    fun <T> new(factory: () -> T) = NewProvider(factory).also { factories[it] = factory }
-    fun <A, T> new(factory: (A) -> T) = NewProvider1(factory).also { factories[it] = factory }
-    fun <A, B, T> new(factory: (A, B) -> T) = NewProvider2(factory).also { factories[it] = factory }
-    fun <A, B, C, T> new(factory: (A, B, C) -> T) = NewProvider3(factory).also { factories[it] = factory }
-    fun <A, B, C, D, T> new(factory: (A, B, C, D) -> T) = NewProvider4(factory).also { factories[it] = factory }
-    fun <A, B, C, D, E, T> new(factory: (A, B, C, D, E) -> T) = NewProvider5(factory).also { factories[it] = factory }
+    fun <T> ShankModule.new(factory: () -> T) = NewProvider(factory).also { factories[it] = factory }
+    fun <A, T> ShankModule.new(factory: (A) -> T) = NewProvider1(factory).also { factories[it] = factory }
+    fun <A, B, T> ShankModule.new(factory: (A, B) -> T) = NewProvider2(factory).also { factories[it] = factory }
+    fun <A, B, C, T> ShankModule.new(factory: (A, B, C) -> T) = NewProvider3(factory).also { factories[it] = factory }
+    fun <A, B, C, D, T> ShankModule.new(factory: (A, B, C, D) -> T) = NewProvider4(factory).also { factories[it] = factory }
+    fun <A, B, C, D, E, T> ShankModule.new(factory: (A, B, C, D, E) -> T) = NewProvider5(factory).also { factories[it] = factory }
 
-    fun <T> scoped(factory: ScopedFactory.() -> T) = ScopedProvider(factory).also { factories[it] = factory }
-    fun <A, T> scoped(factory: ScopedFactory.(A) -> T) = ScopedProvider1(factory).also { factories[it] = factory }
-    fun <A, B, T> scoped(factory: ScopedFactory.(A, B) -> T) = ScopedProvider2(factory).also { factories[it] = factory }
-    fun <A, B, C, T> scoped(factory: ScopedFactory.(A, B, C) -> T) = ScopedProvider3(factory).also { factories[it] = factory }
-    fun <A, B, C, D, T> scoped(factory: ScopedFactory.(A, B, C, D) -> T) = ScopedProvider4(factory).also { factories[it] = factory }
-    fun <A, B, C, D, E, T> scoped(factory: ScopedFactory.(A, B, C, D, E) -> T) = ScopedProvider5(factory).also { factories[it] = factory }
+    fun <T> ShankModule.scoped(factory: ScopedFactory.() -> T) = ScopedProvider(factory).also { factories[it] = factory }
+    fun <A, T> ShankModule.scoped(factory: ScopedFactory.(A) -> T) = ScopedProvider1(factory).also { factories[it] = factory }
+    fun <A, B, T> ShankModule.scoped(factory: ScopedFactory.(A, B) -> T) = ScopedProvider2(factory).also { factories[it] = factory }
+    fun <A, B, C, T> ShankModule.scoped(factory: ScopedFactory.(A, B, C) -> T) = ScopedProvider3(factory).also { factories[it] = factory }
+    fun <A, B, C, D, T> ShankModule.scoped(factory: ScopedFactory.(A, B, C, D) -> T) = ScopedProvider4(factory).also { factories[it] = factory }
+    fun <A, B, C, D, E, T> ShankModule.scoped(factory: ScopedFactory.(A, B, C, D, E) -> T) = ScopedProvider5(factory).also { factories[it] = factory }
 
-    fun <T> singleton(factory: () -> T) = SingletonProvider(factory).also { factories[it] = factory }
-    fun <A, T> singleton(factory: (A) -> T) = SingletonProvider1(factory).also { factories[it] = factory }
-    fun <A, B, T> singleton(factory: (A, B) -> T) = SingletonProvider2(factory).also { factories[it] = factory }
-    fun <A, B, C, T> singleton(factory: (A, B, C) -> T) = SingletonProvider3(factory).also { factories[it] = factory }
-    fun <A, B, C, D, T> singleton(factory: (A, B, C, D) -> T) = SingletonProvider4(factory).also { factories[it] = factory }
-    fun <A, B, C, D, E, T> singleton(factory: (A, B, C, D, E) -> T) = SingletonProvider5(factory).also { factories[it] = factory }
-
-    operator fun invoke(customize: ShankModule.() -> Unit) = also(customize)
+    fun <T> ShankModule.singleton(factory: () -> T) = SingletonProvider(factory).also { factories[it] = factory }
+    fun <A, T> ShankModule.singleton(factory: (A) -> T) = SingletonProvider1(factory).also { factories[it] = factory }
+    fun <A, B, T> ShankModule.singleton(factory: (A, B) -> T) = SingletonProvider2(factory).also { factories[it] = factory }
+    fun <A, B, C, T> ShankModule.singleton(factory: (A, B, C) -> T) = SingletonProvider3(factory).also { factories[it] = factory }
+    fun <A, B, C, D, T> ShankModule.singleton(factory: (A, B, C, D) -> T) = SingletonProvider4(factory).also { factories[it] = factory }
+    fun <A, B, C, D, E, T> ShankModule.singleton(factory: (A, B, C, D, E) -> T) = SingletonProvider5(factory).also { factories[it] = factory }
 }
+operator fun ShankModule.invoke(customize: ShankModule.() -> Unit) = also(customize)
 
