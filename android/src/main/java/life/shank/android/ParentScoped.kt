c58ce751baf55beabf1ef196e3baac88f9c7bac6
Mantas <mantas.varnagiris@gmail.com>
2020-05-13 11:30:44 +0300

    Fixed how ParentScoped works for View

--- android/src/main/java/life/shank/android/ParentScoped.kt
+++ android/src/main/java/life/shank/android/ParentScoped.kt
@@ -6,12 +6,18 @@ import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.view.children
+import androidx.core.view.doOnAttach
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import life.shank.Scope
 import life.shank.Scoped
+import java.util.UUID
+
 
 interface ParentScoped : AutoScoped {
     override fun onScopeReady(block: (Scope) -> Unit) {
@@ -46,38 +52,42 @@ private inline fun Fragment.onParentScopeReadyForFragment(noinline block: (Scope
 }
 
 private inline fun View.onParentScopeReadyForView(noinline block: (Scope) -> Unit) {
-    if (id == View.NO_ID) throw IllegalArgumentException("$this must have an id")
-    val activity = activity ?: throw IllegalArgumentException("$this does not have an Activity")
+    doOnAttach {
+        val activity = activity ?: throw IllegalArgumentException("$this does not have an Activity")
 
-    var scoped: Scoped? = null
-    var autoScoped: AutoScoped? = null
+        var scoped: Scoped? = null
+        var autoScoped: AutoScoped? = null
 
-    var parentView = parent as? View
-    while (parentView != null && scoped == null && autoScoped == null) {
-        when (parentView) {
-            is Scoped -> scoped = parentView
-            is AutoScoped -> autoScoped = parentView
+        var parentView = parent as? View
+        while (parentView != null && scoped == null && autoScoped == null) {
+            when (parentView) {
+                is Scoped -> scoped = parentView
+                is AutoScoped -> autoScoped = parentView
+            }
+            parentView = parentView.parent as? View
         }
-        parentView = parentView.parent as? View
-    }
 
-    if (scoped == null && autoScoped == null && activity is FragmentActivity) {
-        val fragment = activity.supportFragmentManager.findFragmentThatContains(this)
-        val scopedOrAutoScopedFragment = fragment?.findClosestScopedOrAutoScopedFragment()
+        if (scoped == null && autoScoped == null && activity is FragmentActivity) {
+            val tag = getTag(R.id.shank_view_tag_to_find_in_parent) ?: UUID.randomUUID()
+            setTag(R.id.shank_view_tag_to_find_in_parent, tag)
 
-        scoped = scopedOrAutoScopedFragment as? Scoped
-        autoScoped = scopedOrAutoScopedFragment as? AutoScoped
-    }
+            val fragment = activity.supportFragmentManager.findFragmentThatContains(this)
+            val scopedOrAutoScopedFragment = fragment?.findClosestScopedOrAutoScopedFragment()
 
-    if (scoped == null && autoScoped == null) {
-        scoped = activity as? Scoped
-        autoScoped = activity as? AutoScoped
-    }
+            scoped = scopedOrAutoScopedFragment as? Scoped
+            autoScoped = scopedOrAutoScopedFragment as? AutoScoped
+        }
 
-    when {
-        scoped != null -> block(scoped.scope)
-        autoScoped != null -> autoScoped.onScopeReady(block)
-        else -> throw IllegalStateException("$this does not have any parent with Scope")
+        if (scoped == null && autoScoped == null) {
+            scoped = activity as? Scoped
+            autoScoped = activity as? AutoScoped
+        }
+
+        when {
+            scoped != null -> block(scoped.scope)
+            autoScoped != null -> autoScoped.onScopeReady(block)
+            else -> throw IllegalStateException("$this does not have any parent with Scope")
+        }
     }
 }
 
@@ -97,10 +107,16 @@ private fun FragmentManager.findFragmentThatContains(view: View): Fragment? {
     }
 }
 
-private inline fun Fragment.contains(view: View) = view == this.view?.findViewById(view.id)
+private inline fun Fragment.contains(view: View) = view == this.view
+    ?.findViewWithTag(R.id.shank_view_tag_to_find_in_parent, view.getTag(R.id.shank_view_tag_to_find_in_parent))
+
+fun View.findViewWithTag(@IdRes idRes: Int, tag: Any): View? {
+    if (this.getTag(idRes) == tag) return this
+    if (this !is ViewGroup) return null
+    return children.firstOrNull { it.findViewWithTag(idRes, tag) != null }
+}
 
 private val View.activity: Activity? get() = context.activity
-
 private val Context.activity: Activity?
     get() = when (this) {
         is Activity -> this
